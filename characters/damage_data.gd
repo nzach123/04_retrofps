71e18f2dbb211b4d9265fbd96054b115a3ffee54
Nick Zacharkiw <nzach123@live.com>
2024-08-25 18:05:31 -0600

    Player movement and weapon manager

---
+++ characters/damage_data.gd
@@ -0,0 +1,5 @@
+extends Node
+
+class_name DamageData
+
+var amount = 1
---
+++ characters/player/weapon_manager.gd
@@ -0,0 +1,50 @@
+extends Node3D
+
+
+@onready var weapons = $Weapons.get_children()
+
+var weapons_unlocked = []
+var cur_slot = 0
+var cur_weapon = null
+
+func _ready() -> void:
+	disable_all_weapons()
+	for _i in range(weapons.size()):
+		weapons_unlocked.append(false)
+		
+		
+func disable_all_weapons():
+	for weapon in weapons:
+		if has_method("set_active"):
+			weapon.set_active(false)
+		else:
+			weapon.hide()
+	
+func switch_to_previous_weapon():
+	for i in range(weapons.size()):
+		var wrapped_ind = wrapi(cur_slot - 1 - i, 0, weapons.size())
+		if switch_to_weapon_slot(wrapped_ind):
+			break
+	
+	
+func switch_to_next_weapon():
+	for i in range(weapons.size()):
+		var wrapped_ind = wrapi(cur_slot - 1 + i, 0, weapons.size())
+		if switch_to_weapon_slot(wrapped_ind):
+			break
+	
+func switch_to_weapon_slot(slot_ind: int)->bool:
+	if slot_ind > weapons.size() or slot_ind < 0:
+		return false
+	if weapons_unlocked.size() == 0 or !weapons_unlocked[slot_ind]:
+		return false
+	disable_all_weapons()
+	cur_slot = slot_ind
+	cur_weapon = weapons[cur_slot]
+	if has_method("set_active"):
+		cur_weapon.set_active(true)
+	else:
+		cur_weapon.show()
+	
+	
+	return true
